4630da9c002c9f2b61ca3b50fdb7f2c3616e7aed
Dieter Bingemann <dbingema@users.noreply.github.com>
2021-01-31 12:33:06 +0100

    Maschinen Menu auf weissen Hintergrund mit kleineren Icons
    So dass die Menu Optionen anders aussehen als die eigentlichen Maschinen. Habe auch mit Schatten gespielt, aber war nicht zufrieden mit dem Look

--- machineMenu.py
+++ machineMenu.py
@@ -29,23 +29,20 @@ class MachineMenu(cocos.layer.Layer):
         # Positionen
         x1, y1, x2, y2 = self.conveyorInfo['Corners']
         self.center_x, self.center_y = (x1+x2)/2, (y1+y2)/2
-        cancelDimensions = 16
         cancel = cocos.sprite.Sprite('cancel.png', position=(self.center_x, self.center_y), opacity=200)
         self.add(cancel)
 
-        circle = cocos.sprite.Sprite('blackCircle.png', position=(self.center_x, self.center_y), opacity=100, scale=0.7)
+        circle = cocos.sprite.Sprite('whiteCircle.png', position=(self.center_x, self.center_y), opacity=80, scale=0.7)
         self.add(circle)
             
-        # fuer jede Option plus Cancel ein Ding in die Liste einbauen:
- #       self.machineOptions = {"cancel": (x-cancelDimensions, y-cancelDimensions, x+cancelDimensions, y+cancelDimensions), 
- #                              "machine1": (x+offsetRight-w2, y-h2, x+offsetRight+w2, y+h2)}
+        w2, h2 = cancel.width/2, cancel.height/2
+        self.machineOptions = {"cancel": (self.center_x-w2, self.center_y-h2, self.center_x+w2, self.center_y+h2)}
         
-        self.machineOptions = {"cancel": (self.center_x-cancelDimensions, self.center_y-cancelDimensions, 
-                                          self.center_x+cancelDimensions, self.center_y+cancelDimensions)}
         numMachines = len(self.levelInfo.machines)
-        menuRadius = 40
+        menuRadius = 60
 
         angleIncrease = 2*math.pi/numMachines
+##        shadowOffset = 2
 
         for index, machine in enumerate(self.levelInfo.machines):
             # liste enthaelt die images: machine = ein pfad und bild
@@ -54,11 +51,17 @@ class MachineMenu(cocos.layer.Layer):
             w2, h2 = image.width/2, image.height/2
             # bild hin malen
             # neue position 
-            xi = self.center_x + menuRadius * math.cos(index * angleIncrease + 0.5*math.pi)
-            yi = self.center_y + menuRadius * math.sin(index * angleIncrease + 0.5*math.pi)
-            self.add(cocos.sprite.Sprite(image, (xi, yi)))
-            # optin hinzufuegen
             # durch winkel und radius position rechnen
+            xi = self.center_x + menuRadius * math.cos(index * angleIncrease + 0.25*math.pi)
+            yi = self.center_y + menuRadius * math.sin(index * angleIncrease + 0.25*math.pi)
+            # kleines bild hinzufuegen
+            option = cocos.sprite.Sprite(image, (xi, yi), scale = 0.6)
+##            shadow = cocos.sprite.Sprite(image, (xi-shadowOffset, yi+shadowOffset),
+##                                         scale = 0.7, color = (0,0,0), opacity = 50)
+##            self.add(shadow)
+            self.add(option)
+            # optin hinzufuegen zu liste mit file namen als key
+            w2, h2 = option.width/2, option.height/2
             self.machineOptions[machine] = (xi-w2, yi-h2, xi+w2, yi+h2)
             
         
