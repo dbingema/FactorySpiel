92ccac75992e33af0fa92bf530e5c7585cfaf463
Dieter Bingemann <dbingema@users.noreply.github.com>
2021-02-02 19:12:29 +0100

    Einzel Program File entfernt, Ideen als neues File abgespeichert

---
+++ Ideen.py
@@ -0,0 +1,39 @@
+#Was wir als naechstes machen sollen:
+# tips / tutorial
+# Geld System / transport
+
+# Machine:
+# - upgrades
+# - vielleicht ein bisschen groesser machen?
+# - verschiedene typen von maschinen z.B.: holz maschine (was wir jetzt haben), elektronik maschine, stoff maschine, eisen maschine` etc...
+
+# Extra / Details:
+
+# jedes verkaufte produkt gibt einen Score (Stuhl = 1, Fernseher = 5 ...), zaehlt hoch
+# produkt gibt auch Geld, aber Geld gibt man aus fuer upgrades und up-level/andere Produkte
+# man verliert, wenn man auf 0 Euro gesunken ist und keine Produkte mehr hat zum verkaufen
+# zeitdruck, timer. man darf nicht zu lange warten.
+#   z.B.: im laufe der zeit bringt das produkt aber immer weniger geld (Anzeigen wie thermometer),
+#       roh stoff kostet aber immer das gleiche
+# verschidene levels haben ein bestimmten terrain
+
+# Spielverlauf:
+#   - rohstoff lager am anfang des fliessbandes
+#   - durch click fuellt sich lager (z.B. 10 neue rohstoffe), kostet dann geld
+#   - geht aber auch nicht immer, liefer schwierigkeiten oder liefer dauer
+#   - oder es kostet mehr, wenn man es schneller haben will
+#   - roh stoff wird automatisch aus lager auf fliessband geleget, zufaelliger Abstand
+#   - reload delay, rohstoff rate (aufs band) und preise fuer rohstoff und produkt so dass es knapp ist
+#   - wenn genug geld eingenommen, kann man schnellere Maschinen kaufen (upgrades option)
+#   - upgrade ist billiger als neue Maschine
+
+
+# Man kann aber auch auf ein neues level gehen:
+#   - transition effekt
+#   - neuen level (anderes product, schnelleres / andere form fliessband, etc...)
+#   - mehrere Stufen fuer Produkt, verschiedene Farben Rohstoff/Maschinen
+#   - naechstes level selbst entsheiden wenn man genug geld hat
+
+#Ziel:
+#   - gewinnt das letzte level
+#   - oder einfach nur high score bekommen? -- irgenwann verliert man, es wird immer schwieriger/schneller
