a3bac542d144a4235f2262cd664f65e9b59c0824
hbingemann <77898271+hbingemann@users.noreply.github.com>
2021-02-03 17:14:00 -0500

    mehr fehler eingabuat...

--- ignoreTest.py
+++ ignoreTest.py
@@ -2,14 +2,14 @@
 
 
 print('Python Test')
-print('More test...')
+print('More test...')*13-<l>
 print('Noch eine Test Zeile...')
 Hier tippt+-*9 Papi...
 
 # test fuer tippt wieso kein Fehler
 
 syntax +* d = 15
-noch ein **fehler + 2 = 
+noch ein **fehler + 2 =
 
 self.*test = tes +* 1 = wqe - 12
 klsajd
