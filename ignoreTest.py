fb22ebfe15d2f420c6ecbcb0b61a7669ec88540f
Dieter Bingemann <dbingema@users.noreply.github.com>
2021-02-03 22:36:23 +0100

    mehr tests

--- ignoreTest.py
+++ ignoreTest.py
@@ -1,7 +1,11 @@
-print('Python Test')
+"""Test"""
+
+
+print('Python Test') jkdfshjk
 print('More test...') - und hier tippt Henrik...
 print('Noch eine Test Zeile...')
 Hier tippt Papi...
-test fuer tippt
+
+# test fuer tippt wieso kein Fehler
 
 syntax
