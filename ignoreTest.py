e951f0c4f6e95b8b6d3a34d24fd09d15d2e2c778
Dieter Bingemann <dbingema@users.noreply.github.com>
2021-02-05 23:41:29 +0100

    neues test file

--- ignoreTest.py
+++ ignoreTest.py
@@ -6,18 +6,11 @@ print('Noch eine Test Zeile...')
 Hier = tippt * Papi**2
 
 # test fuer tippt wieso kein Fehler
-l;kdsf 0943 89 34;lsadkr09
-idj fioerje if134t0y7t]8
-
-lfj  rej4309 854;lefkw
+# test fuer tippt wieso kein Fehler
+testing testing
 
 
-syntax +* d = 15
-noch ein **<(&3fehler + 2 =lfdsk _0(*Y)
-
-sda,lk(*^$W*     $#@)( )
-kjd_)$(#@)*$B
-
+self.*test = tes +* 1 = wqe - 12kdsj
 self.*test = tes +* 1 = wqe - 12kdsj
         klsajd+9":><_Q@(J*$B $# )
 
