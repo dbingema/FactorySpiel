20df3b0814e5b3fc6ec7faaff41c8cf8b3fdf855
Dieter Bingemann <dbingema@users.noreply.github.com>
2021-02-12 23:05:07 +0100

    Neue Factory Map

--- img/factoryTileSetMoreBoxes.tsx
+++ img/factoryTileSetMoreBoxes.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="factoryTileSetMoreBoxes" tilewidth="64" tileheight="64" tilecount="20" columns="4">
+<tileset version="1.2" tiledversion="1.3.4" name="backgroundTilesImgMoreBoxes" tilewidth="64" tileheight="64" tilecount="20" columns="4">
  <image source="backgroundTilesImgMoreBoxes.png" width="256" height="320"/>
 </tileset>
